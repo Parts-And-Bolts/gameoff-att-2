3379810e334ce247adeb552fdbaaa93a8cd84bd7
Parts-And-Bolts <danial1310201@gmail.com>
2024-11-19 19:31:17 +0800

    added aiming indicator and fixed resolution related bugs

--- Scripts/golf-ball.gd
+++ Scripts/golf-ball.gd
@@ -20,13 +20,15 @@ func _ready():
 
 var input = false
 
+
+
 func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("shoot"):
 		LastHold = global_position
 		
 	if event.is_action_released("shoot") and input and get_global_mouse_position().distance_to(LastHold) > 1:
-		var currentSpeed = clampf(get_global_mouse_position().distance_to(LastHold)*2, 0, speed)
+		var currentSpeed = clampf(get_global_mouse_position().distance_to(LastHold)*5, 0, speed)
 		velocity = get_global_mouse_position().direction_to(LastHold)*currentSpeed
 		horizontal.target_position.x = (get_global_mouse_position().direction_to(LastHold).x)* raycastDistance
 		vertical.target_position.y = (get_global_mouse_position().direction_to(LastHold).y)* raycastDistance
@@ -39,7 +41,13 @@ func _physics_process(delta: float) -> void:
 	input = (convert(velocity.x,TYPE_INT) == 0)
 	
 	
-		
+	trail.visible = input
+	if input:
+		if trail.polygon[1] != null:
+			trail.polygon[1].x = clampf(get_global_mouse_position().distance_to(global_position)*5,0,speed)
+			trail.polygon[2].x = clampf(get_global_mouse_position().distance_to(global_position)*5,0,speed)
+			
+	trail.look_at(global_position + -global_position.direction_to(get_global_mouse_position()))
 	
 	if horizontal.is_colliding():
 		velocity.x *= -1
